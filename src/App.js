c4f51c2575318a996875fe770045d2988904c567
Bora Manga <bora.mng525@gmail.com>
2020-03-30 22:51:14 +0200

    animated bar chart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Circles from "./Graphs/Circles";
 import Lines from "./Graphs/Lines";
 import LinesAxis from "./Graphs/LinesWithAxis";
+import AnimatedGraph from "./Graphs/animated_graph";
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
       </div>
       <div className="svgBox col-3">
         <LinesAxis />
+      </div>
+      <div className="svgBox col-3">
+        <AnimatedGraph />
       </div>
     </React.Fragment>
   );
--- src/Graphs/animated_graph.js
+++ src/Graphs/animated_graph.js
@@ -1,27 +1,24 @@
 import React, { useRef, useEffect, useState } from "react";
-import {
-  select,
-  line,
-  curveCardinal,
-  axisBottom,
-  axisLeft,
-  scaleLinear
-} from "d3";
+import { select, axisBottom, axisLeft, scaleLinear, scaleBand } from "d3";
 
-function LinesAxis() {
+function AnimatedGraph() {
   const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
   const svgRef = useRef();
   useEffect(() => {
     const svg = select(svgRef.current);
-    const xScale = scaleLinear()
-      .domain([0, data.length - 1])
-      .range([0, 300]); //Range is here like svg width to fit in it
+    const xScale = scaleBand()
+      .domain([0, 1, 2, 3, 4, 5, 6])
+      .range([0, 300]) //Range is here like svg width to fit in it
+      .padding(0.2);
     const yScale = scaleLinear()
       .domain([0, 150])
       .range([150, 0]); //to make upside down
-    const xAxis = axisBottom(xScale)
-      .ticks(data.length)
-      .tickFormat(index => index + 1);
+    const colorScale = scaleLinear()
+      .domain([10, 70, 150])
+      .range(["yellow", "orange", "red"])
+      .clamp(true); //belirli bir degerden sonra renk degistir. Burada 30 mesela
+
+    const xAxis = axisBottom(xScale).ticks(data.length);
     const yAxis = axisLeft(yScale);
     svg.select(".y-axis").call(yAxis);
 
@@ -29,18 +26,18 @@ function LinesAxis() {
       .select(".x-axis")
       .style("transform", "translateY(150px)")
       .call(xAxis);
-    const myLine = line()
-      .x((value, index) => xScale(index))
-      .y(yScale)
-      .curve(curveCardinal);
     svg
-      .selectAll(".line")
-      .data([data])
-      .join("path")
-      .attr("class", "line")
-      .attr("d", myLine)
-      .attr("fill", "none")
-      .attr("stroke", "blue");
+      .selectAll(".bar")
+      .data(data)
+      .join("rect")
+      .attr("class", "bar")
+      .style("transform", "scale(1, -1)")
+      .attr("x", (value, index) => xScale(index))
+      .attr("y", -150)
+      .attr("width", xScale.bandwidth())
+      .transition()
+      .attr("fill", colorScale)
+      .attr("height", value => 150 - yScale(value));
   }, [data]);
   console.log("data", data);
 
@@ -63,4 +60,4 @@ function LinesAxis() {
   );
 }
 
-export default LinesAxis;
+export default AnimatedGraph;
